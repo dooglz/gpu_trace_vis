8a6b340dfb805db9fbdfe1e6e1d200ff5930271b
Sam Serrels <s.serrels@napier.ac.uk>
2016-07-27 13:48:57 +0100

    Straining the spagetti

--- js/main.js
+++ js/main.js
@@ -1,238 +1,246 @@
 
-$(document).ready(function () { GetFilesList();});
+/* global GetFilesList InitVis*/
+/* exported InitVis*/
+
+$(document).ready(function() {
+  GetFilesList();
+});
 
 class ComputeUnitOccupancy {
-    constructor(cu, startTick, occ) {
-		this.occ = occ;
-		//used for vis
-		this.start = startTick;
-        this.end = startTick + 1;
-		this.lane = cu;
-    }
+  constructor(cu, startTick, occ) {
+    this.occ = occ;
+    this.start = startTick;
+    this.end = startTick + 1;
+    this.lane = cu;
+  }
 }
 
 class InstructionInstance {
-    constructor(id, cu, wf, startTick, name, type) {
-        this.id = id;
-		this.cu = cu;
-		this.wf = wf;
-        this.name = name;
-		this.type = type;
-		//used for vis
-		this.start = startTick;
-        this.end = startTick + 1;
-		this.lane = wf;
-    }
+  constructor(id, cu, wf, startTick, name, type) {
+    this.id = id;
+    this.cu = cu;
+    this.wf = wf;
+    this.name = name;
+    this.type = type;
+    this.start = startTick;
+    this.end = startTick + 1;
+    this.lane = wf;
+  }
 }
 class InstructionMetric {
-    constructor(name, ticks) {
-        this.name = name;
-		this.count = 1;
-		this.ticks = [ticks];
-		this.tickTotal = ticks;
-    }
+  constructor(name, ticks) {
+    this.name = name;
+    this.count = 1;
+    this.ticks = [ticks];
+    this.tickTotal = ticks;
+  }
 }
 
-function Loadfies(name){
- let jqxhr = $.get("data/"+name, function (data2) {
-		console.log(data2.substring(0, 200));
-		ParseTrace(data2);
-		CalcMetrics();
-		InitVis();
-	});
-	jqxhr.fail(function (e) {
-		console.log("error", e);
-	});
+function Init(file) {
+  console.log(file.substring(0, 200));
+  ParseTrace(file);
+  CalcMetrics();
+  InitVis();
 }
 
-
-function GetFilesList() {
-  let jqxhr = $.getJSON("files.php", function (data) {
-	  console.log(data);
-	  data.forEach(function(element) {
-		  $('<button/>').text(element).click(function () { Loadfies(element); }).appendTo($("#filesDiv"));
-	  }, this);
+function OpenFileFromXHR(name) {
+  let jqxhr = $.get("data/" + name, function(data2) {
+    Init(name);
   });
-  jqxhr.fail(function (e) {
+  jqxhr.fail(function(e) {
     console.log("error", e);
   });
 }
 
-var openFile = function (event) {
-	var input = event.target;
+function GetFilesList() {
+  let jqxhr = $.getJSON("files.php", function(data) {
+    console.log(data);
+    data.forEach(function(element) {
+      $('<button/>').text(element).click(function() {
+        OpenFileFromXHR(element);
+      }).appendTo($("#filesDiv"));
+    }, this);
+  });
+  jqxhr.fail(function(e) {
+    console.log("error", e);
+  });
+}
 
-	var reader = new FileReader();
-	reader.onload = function () {
-		var text = reader.result;
-		console.log(reader.result.substring(0, 200));
-		ParseTrace(reader.result);
-		CalcMetrics();
-		InitVis();
-	};
-	reader.readAsText(input.files[0]);
-};
+function openFileFromDisk(event) {
+  var input = event.target;
+  var reader = new FileReader();
+  reader.onload = function() {
+    Init(reader.result);
+  };
+  reader.readAsText(input.files[0]);
+}
 
-var parseToObj = function (line) {
-	var v = false;
-	var obj = {}
-	var ks = "";
-	var vs = "";
-	[...line].forEach(c => {
-		switch (c) {
-			case " ":
-				v = false;
-				obj[ks.replace(/['"]+/g, '')] = vs.replace(/['"]+/g, '');
-				ks = "";
-				vs = "";
-				break;
-			case "=":
-				v = true;
-				break;
-			default:
-				if (v) {
-					vs += c;
-				} else {
-					ks += c;
-				}
-				break;
-		}
-	});
-	obj[ks.replace(/['"]+/g, '')] = vs.replace(/['"]+/g, '');
-	obj.cu = parseInt(obj.cu);
-	obj.wf = parseInt(obj.wf);
-	return obj;
+var parseToObj = function(line) {
+  var v = false;
+  var obj = {};
+  var ks = "";
+  var vs = "";
+  [...line].forEach(c => {
+    switch (c) {
+      case " ":
+        v = false;
+        obj[ks.replace(/['"]+/g, '')] = vs.replace(/['"]+/g, '');
+        ks = "";
+        vs = "";
+        break;
+      case "=":
+        v = true;
+        break;
+      default:
+        if (v) {
+          vs += c;
+        } else {
+          ks += c;
+        }
+        break;
+    }
+  });
+  obj[ks.replace(/['"]+/g, '')] = vs.replace(/['"]+/g, '');
+  obj.cu = parseInt(obj.cu, 10);
+  obj.wf = parseInt(obj.wf, 10);
+  return obj;
 }
 
 var Instuctions;
 var metrics;
-var ParseTrace = function (trace) {
-	var allTextLines = [];
-	allTextLines = trace.split(/\r\n|\n/);
-	trace = "";
-	var clock = 0;
-	var startTick =-1;
-	var mostCU=0;
-	var mostWF=0;
-	Instuctions = [];
-	metrics = {};
-	metrics.wavefronts = [];
-	allTextLines.forEach(v => {
-		if (v.startsWith("c clk=")) {
-			//clock
-			clock = parseInt(v.substring(6));
-			if (startTick === -1){startTick = clock;}
-		} else if (v.startsWith("si.new_inst")) {
-			let oo = parseToObj(v);
-			mostCU =  oo.cu > mostCU ? oo.cu : mostCU;
-			mostWF =  oo.wf > mostWF ? oo.wf : mostWF;
-			if(metrics.wavefronts[oo.wf] === undefined){
-				metrics.wavefronts[oo.wf] = {};
-				metrics.wavefronts[oo.wf].cu = oo.cu;
-			}
-			let type = detemineAsmType(oo.asm);
-			//Instuctions.set(oo.id + "_" + oo.cu, new InstructionInstance(oo.id, oo.cu, oo.wf, clock, oo.asm, type));
-			Instuctions.push(new InstructionInstance(oo.id, oo.cu, oo.wf, clock, oo.asm, type));
-		} else if (v.startsWith("si.inst")) {
-			//parseToObj(v);
-		} else if (v.startsWith("si.end_inst")) {
-			var oo = parseToObj(v);
-			var inst = Instuctions.find((it) => { return (oo.id == it.id && oo.cu == it.cu) });
-			if (inst === undefined) {
-				console.error(oo.id + "_" + oo.cu);
-			} else {
-				inst.end = clock;
-			}
-		}
-	});
+function ParseTrace(trace) {
+  var allTextLines = [];
+  allTextLines = trace.split(/\r\n|\n/);
+  trace = "";
+  var clock = 0;
+  var startTick = -1;
+  var mostCU = 0;
+  var mostWF = 0;
+  Instuctions = [];
+  metrics = {};
+  metrics.wavefronts = [];
+  allTextLines.forEach(v => {
+    if (v.startsWith("c clk=")) {
+      // clock
+      clock = parseInt(v.substring(6), 10);
+      if (startTick === -1) {
+        startTick = clock;
+      }
+    } else if (v.startsWith("si.new_inst")) {
+      let oo = parseToObj(v);
+      mostCU = oo.cu > mostCU ? oo.cu : mostCU;
+      mostWF = oo.wf > mostWF ? oo.wf : mostWF;
+      if (metrics.wavefronts[oo.wf] === undefined) {
+        metrics.wavefronts[oo.wf] = {};
+        metrics.wavefronts[oo.wf].cu = oo.cu;
+      }
+      let type = detemineAsmType(oo.asm);
+      //Instuctions.set(oo.id + "_" + oo.cu, new InstructionInstance(oo.id, oo.cu, oo.wf, clock, oo.asm, type));
+      Instuctions.push(new InstructionInstance(oo.id, oo.cu, oo.wf, clock, oo.asm, type));
+    } else if (v.startsWith("si.inst")) {
+      //parseToObj(v);
+    } else if (v.startsWith("si.end_inst")) {
+      var oo = parseToObj(v);
+      var inst = Instuctions.find(it => {
+        return (oo.id === it.id && oo.cu === it.cu);
+      });
+      if (inst === undefined) {
+        console.error(oo.id + "_" + oo.cu);
+      } else {
+        inst.end = clock;
+      }
+    }
+  });
 
-	metrics.wfCount = mostWF+1;
-	metrics.cuCount = mostCU+1;
-	metrics.cu = new Array(mostCU+1);  
-	metrics.startTick = startTick;
-	metrics.endTick = clock;
-};
+  metrics.wfCount = mostWF + 1;
+  metrics.cuCount = mostCU + 1;
+  metrics.cu = new Array(mostCU + 1);
+  metrics.startTick = startTick;
+  metrics.endTick = clock;
+}
+
 var cuocc;
-var CalcMetrics = function (trace) {
-	for(let i = 0; i < metrics.cu.length; i++){
-		metrics.cu[i] = {};
-		metrics.cu[i].asms = new Map();
-		metrics.cu[i].occupancy = [new ComputeUnitOccupancy(i,metrics.startTick,0)];	
-		metrics.cu[i].rawOccupancy = new Array(metrics.endTick+1 - metrics.startTick);
-		metrics.cu[i].rawOccupancy.fill(0); 
-		metrics.cu[i].wfActivity = new Array(metrics.endTick+1 - metrics.startTick);
-		metrics.cu[i].wfboolActivity = new Array(metrics.endTick+1 - metrics.startTick);
-		metrics.cu[i].maxWfAcitivity =0;
-		metrics.cu[i].maxboolWfAcitivity =0;
-		metrics.cu[i].maxRawWfAcitivity =0;
-		for(let k = 0; k < metrics.cu[i].wfActivity.length; k++){
-			metrics.cu[i].wfActivity[k] = new Array(metrics.wfCount);
-			metrics.cu[i].wfActivity[k].fill(0);
-			metrics.cu[i].wfboolActivity[k] = new Array(metrics.wfCount);
-			metrics.cu[i].wfboolActivity[k].fill(false);
-		}
-	}
+function CalcMetrics(trace) {
+  for (let i = 0; i < metrics.cu.length; i++) {
+    metrics.cu[i] = {};
+    metrics.cu[i].asms = new Map();
+    metrics.cu[i].occupancy = [new ComputeUnitOccupancy(i, metrics.startTick, 0)];
+    metrics.cu[i].rawOccupancy = new Array(metrics.endTick + 1 - metrics.startTick);
+    metrics.cu[i].rawOccupancy.fill(0);
+    metrics.cu[i].wfActivity = new Array(metrics.endTick + 1 - metrics.startTick);
+    metrics.cu[i].wfboolActivity = new Array(metrics.endTick + 1 - metrics.startTick);
+    metrics.cu[i].maxWfAcitivity = 0;
+    metrics.cu[i].maxboolWfAcitivity = 0;
+    metrics.cu[i].maxRawWfAcitivity = 0;
+    for (let k = 0; k < metrics.cu[i].wfActivity.length; k++) {
+      metrics.cu[i].wfActivity[k] = new Array(metrics.wfCount);
+      metrics.cu[i].wfActivity[k].fill(0);
+      metrics.cu[i].wfboolActivity[k] = new Array(metrics.wfCount);
+      metrics.cu[i].wfboolActivity[k].fill(false);
+    }
+  }
 
-	Instuctions.forEach(i => {
-		//oocupancy
-		for(let j = (i.start - metrics.startTick); j < ((i.end-1) - metrics.startTick); j++){
-			metrics.cu[i.cu].rawOccupancy[j]++;
-			metrics.cu[i.cu].wfActivity[j][i.wf]++;
-			metrics.cu[i.cu].wfboolActivity[j][i.wf] = true;
-			metrics.cu[i.cu].maxboolWfAcitivity = Math.max(metrics.cu[i.cu].maxboolWfAcitivity,metrics.cu[i.cu].wfboolActivity[j].reduce(function (a, b) {
-				return a + b;
-			} ));
-			metrics.cu[i.cu].maxWfAcitivity = Math.max(metrics.cu[i.cu].maxboolWfAcitivity,metrics.cu[i.cu].wfActivity[j][i.wf] );
-			metrics.cu[i.cu].maxRawWfAcitivity = Math.max(metrics.cu[i.cu].maxRawWfAcitivity,metrics.cu[i.cu].rawOccupancy[j]);
-		}
-		if (metrics.cu[i.cu].asms.has(i.name)) {
-			let mm = metrics.cu[i.cu].asms.get(i.name);
-			mm.count++;
-			mm.ticks.push(i.end - i.start);
-			mm.tickTotal += (i.end - i.start);
-		} else {
-			metrics.cu[i.cu].asms.set(i.name, new InstructionMetric(i.name, i.end - i.start));
-		}
-	});
-	let mostCalled = 0;
-	let Expesnive = 0;
-	let ExpesniveA = 0;
-	let mostTicks = 0;
-	metrics.cu.forEach(m => {
-		m.asms.forEach(im => {
-			if (mostCalled === 0 || im.count > mostCalled.count) {
-				mostCalled = im;
-			}
-			if (mostCalled === 0 || im.count > mostCalled.count) {
-				mostCalled = im;
-			}
-			let avg = im.tickTotal / (im.ticks.length || 1);
-			if (mostCalled === 0 || avg > ExpesniveA) {
-				ExpesniveA = avg;
-				Expesnive = im;
-			}
-			if (mostTicks === 0 || im.tickTotal > mostTicks.tickTotal) {
-				mostTicks = im;
-			}
-		})
-	});
-	console.log(`Most called:`, mostCalled, mostCalled.count);
-	console.log(`Most Expesnive call:`, Expesnive, ExpesniveA);
-	console.log(`Longest Running:`, mostTicks, mostTicks.tickTotal);
+  Instuctions.forEach(i => {
+    //oocupancy
+    for (let j = (i.start - metrics.startTick); j < ((i.end - 1) - metrics.startTick); j++) {
+      metrics.cu[i.cu].rawOccupancy[j]++;
+      metrics.cu[i.cu].wfActivity[j][i.wf]++;
+      metrics.cu[i.cu].wfboolActivity[j][i.wf] = true;
+      metrics.cu[i.cu].maxboolWfAcitivity = Math.max(metrics.cu[i.cu].maxboolWfAcitivity, metrics.cu[i.cu].wfboolActivity[j].reduce(function(a, b) {
+        return a + b;
+      }));
+      metrics.cu[i.cu].maxWfAcitivity = Math.max(metrics.cu[i.cu].maxboolWfAcitivity, metrics.cu[i.cu].wfActivity[j][i.wf]);
+      metrics.cu[i.cu].maxRawWfAcitivity = Math.max(metrics.cu[i.cu].maxRawWfAcitivity, metrics.cu[i.cu].rawOccupancy[j]);
+    }
+    if (metrics.cu[i.cu].asms.has(i.name)) {
+      let mm = metrics.cu[i.cu].asms.get(i.name);
+      mm.count++;
+      mm.ticks.push(i.end - i.start);
+      mm.tickTotal += (i.end - i.start);
+    } else {
+      metrics.cu[i.cu].asms.set(i.name, new InstructionMetric(i.name, i.end - i.start));
+    }
+  });
+  let mostCalled = 0;
+  let Expesnive = 0;
+  let ExpesniveA = 0;
+  let mostTicks = 0;
+  metrics.cu.forEach(m => {
+    m.asms.forEach(im => {
+      if (mostCalled === 0 || im.count > mostCalled.count) {
+        mostCalled = im;
+      }
+      if (mostCalled === 0 || im.count > mostCalled.count) {
+        mostCalled = im;
+      }
+      let avg = im.tickTotal / (im.ticks.length || 1);
+      if (mostCalled === 0 || avg > ExpesniveA) {
+        ExpesniveA = avg;
+        Expesnive = im;
+      }
+      if (mostTicks === 0 || im.tickTotal > mostTicks.tickTotal) {
+        mostTicks = im;
+      }
+    })
+  });
+  console.log(`Most called:`, mostCalled, mostCalled.count);
+  console.log(`Most Expesnive call:`, Expesnive, ExpesniveA);
+  console.log(`Longest Running:`, mostTicks, mostTicks.tickTotal);
 }
 
 var INSTRUCTIONTYPE = {
-	SCALER: { value: 0, name: "Scaler", prefix: "S_" },
-	VECTOR: { value: 1, name: "Vector", prefix: "V_" },
-	DATA: { value: 2, name: "Data Share", prefix: "DS_" },
-	IMAGE: { value: 3, name: "Image Memory", prefix: "IMAGE_" },
-	TBUF: { value: 4, name: "Typed Buffer", prefix: "TBUFFER_" },
+  SCALER: { value: 0, name: "Scaler", prefix: "S_" },
+  VECTOR: { value: 1, name: "Vector", prefix: "V_" },
+  DATA: { value: 2, name: "Data Share", prefix: "DS_" },
+  IMAGE: { value: 3, name: "Image Memory", prefix: "IMAGE_" },
+  TBUF: { value: 4, name: "Typed Buffer", prefix: "TBUFFER_" },
 };
 
-var detemineAsmType = function (ss) {
-	for (var propt in INSTRUCTIONTYPE) {
-		if (ss.startsWith(INSTRUCTIONTYPE[propt].prefix)) {
-			return INSTRUCTIONTYPE[propt];
-		}
-	}
-	console.error("Unkown Inst type: " + ss);
+function detemineAsmType(ss) {
+  for (var propt in INSTRUCTIONTYPE) {
+    if (ss.startsWith(INSTRUCTIONTYPE[propt].prefix)) {
+      return INSTRUCTIONTYPE[propt];
+    }
+  }
+  console.error("Unkown Inst type: " + ss);
 }
--- js/vis.js
+++ js/vis.js
@@ -1,94 +1,85 @@
+/* global d3, INSTRUCTIONTYPE, Instuctions, InitVis, TaskChart, metrics */
+/* exported InitVis*/
+
 let catagoryColourScale20 = d3.scale.category20();
 let catagoryColourScale10 = d3.scale.category10();
-var colourScalefunc = function (d) {
-		if (Object.keys(INSTRUCTIONTYPE).length > 10) {
-			return catagoryColourScale20(d.type.value);
-		} else {
-			return catagoryColourScale10(d.type.value);
-		}
+
+/** Colour by d.type.value*/
+function colourScalefunc(d) {
+  if (Object.keys(INSTRUCTIONTYPE).length > 10) {
+    return catagoryColourScale20(d.type.value);
+  }
+  return catagoryColourScale10(d.type.value);
 }
-var colourCUfunc = function (d) {
-	return catagoryColourScale10(d.cu);
+
+/** Colour by d.cu*/
+function colourCUfunc(d) {
+  return catagoryColourScale10(d.cu);
 }
+
 let showWfCu = false;
 let tc;
 let cuVisSvg = null;
-var InitVis = function () {
-	if(tc){tc.Clear()};
-	if(cuVisSvg){ClearCuvis() ;}
-	tc = new TaskChart(d3.select("#visContainerDiv"), Instuctions, metrics.wfCount);
-	tc.setFillfunc(showWfCu ? colourCUfunc : colourScalefunc);
-	tc.Redraw();
+
+function InitVis() {
+  if (tc) {
+    tc.Clear();
+  }
+  if (cuVisSvg) {
+    ClearCuvis();
+  }
+  tc = new TaskChart(d3.select("#visContainerDiv"), Instuctions, metrics.wfCount);
+  tc.setFillfunc(showWfCu ? colourCUfunc : colourScalefunc);
+  tc.Redraw();
 }
-var maxOcc;
+
 var acitivityMode = false;
 
-var ClearCuvis = function () {
-	cuVisSvg.remove();
+function ClearCuvis() {
+  cuVisSvg.remove();
 }
 
-var ShowCuvis = function () {
-	tc.Clear();
+function ShowCuvis() {
+  tc.Clear();
+  let max = metrics.cu[0].wfboolActivity.length;
+  let min = 0;
+  let padding = [0, 30, 20, 10];
+  let container = d3.select("#visContainerDiv");
+  let width = parseInt(container.style("width"), 10);
+  let height = parseInt(container.style("height"), 10);
+  let numberOfLanes = metrics.cuCount;
+  let xScale = d3.scale.linear().domain([min, max]).range([0, width - (padding[2] + padding[3])]).clamp(true);
+  let yCatScale = d3.scale.ordinal().domain(d3.range(numberOfLanes)).rangeRoundBands([0, height - (padding[0] + padding[1])]);
+  let yScale = d3.scale.ordinal().domain(d3.range(numberOfLanes)).rangeRoundBands([0, height - (padding[0] + padding[1])]);
+  let xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSubdivide(true).tickSize(8).tickPadding(8);
+  let yAxis = d3.svg.axis().scale(yCatScale).orient("left").tickSize(0);
+
+  cuVisSvg = container.append('svg').attr('width', width).attr('height', height);
+  cuVisSvg.append("g").attr("class", "x axis")
+    .attr("transform", "translate(" + padding[2] + ", " + (height - padding[1]) + ")")
+    .transition().call(xAxis);
+  cuVisSvg.append("g").attr("class", "y axis")
+    .attr("transform", "translate(" + padding[2] + ", 0)")
+    .transition().call(yAxis);
 
 
-	let max =  metrics.cu[0].wfboolActivity.length;
-	let min = 0;
+  for (let i = 0; i < metrics.cuCount; i++) {
+    let data = metrics.cu[i];
+    let area = d3.svg.area()
+      .x(function(d, i) {
+        return xScale(i);
+      })
+      .y0(yCatScale.rangeBand())
+      .y1(function(d, i) {
+        return yCatScale.rangeBand() - yScale((acitivityMode ? d : d.reduce(function(a, b) {
+          return a + b;
+        })));
+      });
 
-
-	let padding = [0, 30, 20, 10];
-	let container = d3.select("#visContainerDiv");
-	let width = parseInt(container.style("width"));
-	let height = parseInt(container.style("height"));
-	let numberOfLanes = metrics.cuCount;
-	let xScale = d3.scale.linear().domain([min, max]).range([0, width - (padding[2] + padding[3])]).clamp(true);
-	let yCatScale = d3.scale.ordinal().domain(d3.range(numberOfLanes)).rangeRoundBands([0, height - (padding[0] + padding[1])]);
-	let yScale = d3.scale.ordinal().domain(d3.range(numberOfLanes)).rangeRoundBands([0, height - (padding[0] + padding[1])]);
-	let xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSubdivide(true).tickSize(8).tickPadding(8);
-	let yAxis = d3.svg.axis().scale(yCatScale).orient("left").tickSize(0);
-
-	cuVisSvg = container.append('svg').attr('width', width).attr('height', height);
-        cuVisSvg.append("g").attr("class", "x axis")
-            .attr("transform", "translate(" + padding[2] + ", " + (height - padding[1]) + ")")
-            .transition().call(xAxis);
-        cuVisSvg.append("g").attr("class", "y axis")
-            .attr("transform", "translate(" + padding[2] + ", 0)")
-            .transition().call(yAxis);
-
-
-	for(let i =0; i < metrics.cuCount; i++){
-	let data = metrics.cu[i];/*
-	for(let i = 0; i < metrics.cu.length; i++){
-		for(let j = 0; j < metrics.cu[i].rawOccupancy.length; j++){
-			data.push({tick:metrics.cu[i]})
-		}
-	}*/
-
-
-	//let min = Instuctions.reduce((previousValue, currentValue) => (previousValue < currentValue.start ? previousValue : currentValue.start) , Instuctions[0].start);
-	//let max = Instuctions.reduce((previousValue, currentValue) => (previousValue > currentValue.end ? previousValue : currentValue.end), Instuctions[0].end);
-
-	if (acitivityMode) {
-		 maxOcc = data.maxRawWfAcitivity;
-	} else {
-		 maxOcc = data.maxboolWfAcitivity;
-	}
-
-	let area = d3.svg.area()
-		.x(function (d, i) { return xScale(i); })
-		.y0(yCatScale.rangeBand())
-		.y1(function (d, i) {
-			return yCatScale.rangeBand() - yScale((acitivityMode ? d : d.reduce(function (a, b) {
-				return a + b;
-			})))
-		})
-
-	cuVisSvg.append("g")
-		.attr("transform", "translate(" + padding[2] + "," + yScale(i) + ")").append("path")
-		.datum(acitivityMode ? data.rawOccupancy : data.wfboolActivity)
-		.attr("class", "area")
-		.attr("d", area);
-
-	//x.domain(d3.extent(data, function (d) { return d.date; }));
-	//y.domain([0, d3.max(data, function (d) { return d.close; })]);
-	}
+    cuVisSvg.append("g")
+      .attr("transform", "translate(" + padding[2] + "," + yScale(i) + ")").append("path")
+      .datum(acitivityMode ? data.rawOccupancy : data.wfboolActivity)
+      .attr("class", "area")
+      .attr("d", area);
+  }
 }
